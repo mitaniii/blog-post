e411ffdd85003ad8b63484aeab59978ebea63273
y.mitani <y.mitani@cybermissions.co.jp>
2022-03-04 16:17:23 +0900

    トップページの外観とナビゲーションバー

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>index</div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app />
+    <v-app-bar app>
+      <v-toolbar-title class="mx-auto">
+        <h1>
+          Blog-Post-App
+        </h1>
+      </v-toolbar-title>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
